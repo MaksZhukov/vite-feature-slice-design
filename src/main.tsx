80a389f728c32872ef9cc18125da5e99e48b6091
Maksim Zhukau <maks_zhukov_97@mail.ru>
2024-07-10 00:33:22 +0300

    Added web vitals

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { onCLS, onINP, onLCP } from 'web-vitals';
 import App from './app/App.tsx';
+
+onCLS(console.log, { reportAllChanges: true });
+onINP(console.log, { reportAllChanges: true });
+onLCP(console.log, { reportAllChanges: true });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
